0c36cc36b64d437b9d3adc0c03116233f5238759
Haiqa461 <2022ee255@student.uet.edu.pk>
2023-09-24 11:51:56 +0500

    Here I upload Pandas sourse codes

---
+++ first.py
@@ -0,0 +1,12 @@
+import pandas 
+dataset = {
+    'Choclates' : ['sneakers','kitkat', 'Dairymilk','milkyway','Mars',],
+    'Stock': [4,7,8,9,4]
+    
+}
+
+mystore = pandas.DataFrame(dataset)
+print("CHOCO WORLD")
+print("\n")
+print(mystore)
+
